c7ded159666c70e2de5d4de401c46f8a57acd7f2
AviationSFO <srw08sf@gmail.com>
2022-01-06 14:05:36 -0800

    beta 0.1 to do list

--- ToDoList/main.lua
+++ ToDoList/main.lua
@@ -1,7 +1,38 @@
 -- To Do List By Steven Weinstein on 1-6-2022
-file = io.open ("list.txt", "w")
-file:write("aaaaaaaah")
-file:close()
-file = io.open ("~/Desktop/mylualearning/ToDoList/list.txt", "r")
-print(file:read())
-local contents = file:read()
+-- Run command: lua ~/Desktop/mylualearning/ToDoList/main.lua
+go = true
+file = io.open("list.txt", "r+")
+print('To Do List by Steven Weinstein Version Beta 0.1\nPress return to start')
+io.read()
+function main ()
+    print("Enter one of the numbers below coresponding to their option\nOptions:\n1 - Read your list\n2 - Append to your list\n3 - Clear your list\n4 - Save and quit")
+    option = io.read()
+    if option == "1" then
+        file = io.open("list.txt", "r")
+        contents = file:read("*a")
+        if contents == nil or contents == "" then
+            contents = "Empty"
+        end
+        print("Your list is:" .. "\n" .. contents)
+        file:close()
+    elseif option == "2" then
+        file = io.open("list.txt", "a")
+        print("What would you like to add:")
+        local append = io.read()
+        local append = append .. "\n"
+        file:write(append)
+        local append = nil
+    elseif option == "3" then
+        file = io.open("list.txt", "w")
+        file:write("")
+        file:close()
+        print("Cleared your list")
+    elseif option == "4" then
+        go = false
+        print("Saving...")
+        print("Quiting...")
+    end
+end
+while go == true do
+    main()
+end
