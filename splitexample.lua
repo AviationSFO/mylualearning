d56c63f2c9650d8ead5ae7ea1b37f263dae8be2f
AviationSFO <srw08sf@gmail.com>
2022-01-13 08:15:03 -0800

    edit split example

--- splitexample.lua
+++ splitexample.lua
@@ -1,4 +1,8 @@
-local example = "lua is great"
-for i in string.gmatch(example, "%S+") do
-   print(i)
+-- local example = "lua is great"
+str = io.read()
+local function split(str)
+   for i in string.gmatch(str, "%S+") do
+      print(i)
+   end
 end
+split(str)
